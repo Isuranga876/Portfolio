e8f96f0fa0280fa09811cc4984894a3651ffaded
isuranga876 <isurangauni876@gmail.com>
2026-02-22 00:36:34 +0530

    Clean main branch

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,4 +1,4 @@
-import profile from "../assets/images/profile.jpg";
+import profile from "../assets/images/profile.jpg";git 
 import SectionFX from "../components/SectionFX";
 
 export default function Home() {
